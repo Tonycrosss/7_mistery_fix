f1157bd75f189e8c5e9a138477fe2c816acdd2be
Tonycrosss <starovoitov.anton@gmail.com>
2017-10-06 18:25:13 +0500

    Update quadratic_equation.py
    Убрал лишний комментарий, изменил структуру на более понятную

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -3,12 +3,12 @@ from math import sqrt
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    # Если Дискриминант меньше 0, то уравнение не имеет действ. корней
     if discriminant < 0:
         return None, None
-    root1 = (-b - sqrt(discriminant)) / (2 * a)
-    root2 = (-b + sqrt(discriminant)) / (2 * a)
-    if discriminant == 0:
+    elif discriminant == 0:
+        root1 = (-b - sqrt(discriminant)) / (2 * a)
         return root1, None
     else:
+        root1 = (-b - sqrt(discriminant)) / (2 * a)
+        root2 = (-b + sqrt(discriminant)) / (2 * a)
         return root1, root2
